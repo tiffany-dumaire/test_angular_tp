121a991d594af325d4c1a5a6d4f51a85160786fe
tiffany-dumaire <tiffany.du89@live.fr>
2021-03-16 09:31:58 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public festivals;
+  ngOnInit(): void {
+    this.festivals = [
+      { name: "FJM2020", tables: 160 },
+      { name: "FJM2018", tables: 80 },
+      { name: "FJM2019", tables: 110 }
+    ];
+  }
+  ngSubmit(): void {
+    this.festivals.add({ name: name, tables: tables });
+  }
 }
